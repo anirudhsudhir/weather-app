88385f9bb30e93defb5be729195b811637fe1ea9
Anirudh Sudhir <137381184+anirudhsudhir@users.noreply.github.com>
2023-09-19 16:18:33 +0530

    Accept city and update display

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-const dataDump = document.getElementById('data-dump')
-let apiKey = "66cab58a00be0e1b64b6ac3f24d0eb2b"
-let cityLatitude = "", cityLongitude = "";
-getWeatherData("Bangalore");
-
 async function getCityData(cityName) {
     let geocodeEndpoint = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`
     let rawLocationData = await fetch(geocodeEndpoint);
@@ -15,7 +10,30 @@ async function getWeatherData(cityName) {
     await getCityData(cityName)
     let weatherEndpoint = `http://api.openweathermap.org/data/2.5/forecast?lat=${cityLatitude}&lon=${cityLongitude}&appid=${apiKey}`
     let rawWeatherData = await fetch(weatherEndpoint)
-    let weatherData = await rawWeatherData.json()
-    let currentTemperature = weatherData["list"][0]["main"]["temp"]
-    dataDump.textContent = currentTemperature
+    weatherData = await rawWeatherData.json()
 }
+
+function fillWeatherData() {
+    let columnValue = 1, columnCount = 0;
+    for (let i = 0; i < weatherData.list.length; i++) {
+        if (++columnCount >= 8) {
+            columnValue++
+            columnCount = 0
+        }
+        let columnSelectorID = `#datacolumn-${columnValue}`
+        const selectedColumn = document.querySelector(columnSelectorID)
+        selectedColumn.textContent = weatherData["list"][i]["main"]["temp"]
+    }
+}
+
+async function searchCity() {
+    await getWeatherData(searchBox.value)
+    fillWeatherData()
+}
+
+const dataDump = document.querySelector('.weatherdata-day')
+let apiKey = "66cab58a00be0e1b64b6ac3f24d0eb2b"
+let cityLatitude, cityLongitude, weatherData;
+const searchBox = document.getElementById('search-box')
+const searchButton = document.getElementById('search-button')
+searchButton.addEventListener('click', searchCity)
